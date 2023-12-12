2087a54a4485f1476dcee26a99397ced80043e81
Sthefanie12 <153650936+Sthefanie12@users.noreply.github.com>
2023-12-12 11:30:39 -0500

    Add files via upload

---
+++ Sthefanie Torres Algoritmo.py
@@ -0,0 +1,69 @@
+# Suma dos números enteros
+numero1 = 40
+numero2 = 60
+suma = numero1 + numero2
+print("La suma de", numero1, "y", numero2, "es:", suma)
+
+# Resta dos números de punto flotante
+numero3 = 18.5
+numero4 = 9.5
+resta = numero3 - numero4
+print("La resta de", numero3, "y", numero4, "es:", resta)
+
+# Multiplica dos números enteros
+numero5 = 10
+numero6 = 40
+multiplicación = numero5 * numero6
+print("La multiplicación de", numero5, "y", numero6, "es:", multiplicación)
+
+# Divide dos números de punto flotante
+numero7 = 30.5
+numero8 = 6.5
+division = numero7 / numero8
+print("La división de", numero7, "y", numero8, "es:", division)
+
+# Nombre completo
+nombre_apellido = "Sthefanie Torres"
+
+# Longitud del nombre_apellido
+longitud_nombre = len(nombre_apellido)
+
+# Imprime en pantalla
+print("El nombre apellido es:", nombre_apellido)
+print("La longitud del nombre apellido es:", longitud_nombre)
+
+# Conversión de tipo de dato entero a flotante
+numero_entero = 100
+numero_flotante = float(numero_entero)
+print("El tipo de dato original es:", type(numero_entero))
+print("El tipo de dato después de la conversión es:", type(numero_flotante))
+
+# Conversión de tipo de dato flotante a entero
+numero_flotante = 40.7
+numero_entero = int(numero_flotante)
+print("El tipo de dato original es:", type(numero_flotante))
+print("El tipo de dato después de la conversión es:", type(numero_entero))
+
+# Operaciones Básicas y Tipos de Datos
+numero1 = 90
+numero2 = 80
+suma = numero1 + numero2
+print("La suma de", numero1, "y", numero2, "es:", suma)
+
+# Manipulación de Cadenas de Texto
+nombre_apellido = "Sthefanie del Cisne Torres Vega"
+longitud_nombre = len(nombre_apellido)
+print("El nombre y apellido es:", nombre_apellido)
+print("La longitud del nombre y apellido es:", longitud_nombre)
+
+# Conversión de Tipos de Datos
+numero_entero = 80
+numero_flotante = 65.4
+
+# Conversión entero a flotante
+numero_flotante_entero = float(numero_entero)
+print("La conversión de", numero_entero, "a flotante es:", numero_flotante_entero)
+
+# Conversión flotante a entero
+numero_entero_flotante = int(numero_flotante)
+print("La conversión de", numero_flotante, "a entero es:", numero_entero_flotante)
